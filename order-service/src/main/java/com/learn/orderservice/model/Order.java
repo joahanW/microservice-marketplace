284cd93d13911f54f3af9cca74cf6156586f1dc7
johan <johanwirasugianto@gmail.com>
2023-01-14 17:42:35 +0700

    Make model in Order-service

---
+++ order-service/src/main/java/com/learn/orderservice/model/Order.java
@@ -0,0 +1,23 @@
+package com.learn.orderservice.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "tb_order")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String orderNumber;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    private List<OrderLineItem> orderLineItems;
+}
---
+++ order-service/src/main/java/com/learn/orderservice/model/OrderLineItem.java
@@ -0,0 +1,23 @@
+package com.learn.orderservice.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "tb_order_line_item")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderLineItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String skuCode;
+    private BigDecimal price;
+    private Integer quantity;
+}
