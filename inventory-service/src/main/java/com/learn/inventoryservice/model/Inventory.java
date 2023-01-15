f5798a54041a12a835189755d3f3f351dbfc0ddb
johan <johanwirasugianto@gmail.com>
2023-01-15 14:05:36 +0700

    Inizial Project for Inventory Service

---
+++ inventory-service/src/main/java/com/learn/inventoryservice/model/Inventory.java
@@ -0,0 +1,22 @@
+package com.learn.inventoryservice.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "tb_inventory")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Inventory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String skuCode;
+    private Integer quantity;
+
+}
---
+++ inventory-service/src/main/java/com/learn/inventoryservice/repository/InventoryRepository.java
@@ -0,0 +1,9 @@
+package com.learn.inventoryservice.repository;
+
+import com.learn.inventoryservice.model.Inventory;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface InventoryRepository extends JpaRepository<Inventory, Long> {
+}
