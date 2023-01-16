54cf5a2b8b1a76d5e0d96a6e69cf0a1e659b029a
johan <johanwirasugianto@gmail.com>
2023-01-16 17:47:42 +0700

    Inizialize ENV for Eureka Spring Cloud

---
+++ microservices-new/discovery-server/src/main/java/com/learn/discoveryserver/DiscoveryServerApplication.java
@@ -0,0 +1,14 @@
+package com.learn.discoveryserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class DiscoveryServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DiscoveryServerApplication.class, args);
+        System.out.println("Discovery Server is Running in 8761.....");
+    }
+}
