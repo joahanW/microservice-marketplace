948aaeca9dc173e4bbe721c6088d0d1615d928e2
johan <johanwirasugianto@gmail.com>
2023-01-16 15:43:30 +0700

    Upgrade logic in Inventory request inStock

--- microservices-new/inventory-service/src/main/java/com/learn/inventoryservice/controller/InventoryController.java
+++ microservices-new/inventory-service/src/main/java/com/learn/inventoryservice/controller/InventoryController.java
@@ -1,9 +1,12 @@
 package com.learn.inventoryservice.controller;
 
+import com.learn.inventoryservice.dto.InventoryResponse;
 import com.learn.inventoryservice.service.InventoryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/inventory")
@@ -12,9 +15,14 @@ public class InventoryController {
 
     private final InventoryService inventoryService;
 
-    @GetMapping("/{sku-code}")
+    // Using PathVariable (only handle 1 Items)
+    // http://localhost:8090/api/inventory/iphone_13
+
+    // Using PathPharam (handle moren than 2 items)
+    // http://localhost:8090/api/inventory?sku-code=iphone-13&sku-code=iphone13-red......
+    @GetMapping
     @ResponseStatus(HttpStatus.OK)
-    public boolean isInstock(@PathVariable("sku-code") String skuCode){
+    public List<InventoryResponse> isInstock(@RequestParam List<String> skuCode){
         return inventoryService.isInStock(skuCode);
     }
 
